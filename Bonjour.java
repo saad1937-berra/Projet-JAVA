17f95ba4baf668a304816a46f602230b677978ac
saad1937-berra <saadberra11@gmail.com>
2026-04-15 19:54:40 +0100

    Initial commit

---
+++ Bonjour.java
@@ -0,0 +1,8 @@
+class Bonjour{
+/* méthode main = point de départ du programme */
+public static void main(String[] args){
+/* Construction d’un objet affichage qui sera affiché */
+String affichage = new String("Bonjour");
+System.out.println(affichage);
+}
+}
