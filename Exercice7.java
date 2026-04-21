8a2823bf0864775513ec63dd63381793d0b2d49c
saad1937-berra <saadberra11@gmail.com>
2026-04-21 17:52:54 +0100

    commit

--- Exercice7.java
+++ Exercice7.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Exercice7 {
+    public static void main(String[]args) {
+        Scanner entrer = new Scanner(System.in);
+        while (true) {
+            String calcul = entrer.nextLine();
+            int longueur = calcul.length();
+            System.out.println(longueur + " caractères");
+
+            if (calcul.equals("exit")) {
+                break;
+            }
+        }
+        entrer.close();
+    }
 }
--- Exercice8.java
+++ Exercice8.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Exercice8 {
+    public static void main(String[]args){
+        Scanner entrer = new Scanner(System.in);
+
+        while (true){
+            String saisie = entrer.nextLine();
+            int longueur = saisie.length();
+            System.out.println(longueur + "caractères");
+
+            if (saisie.equalsIgnoreCase("exit")){
+                break;
+            }
+        }
+    }
 }
